e32eb295172d3ad8febc805ae2e82eeb9037c53d
Phill Johntony <phidjohn@cdw.com>
2021-04-12 16:34:22 -0400

    Pagination WIP

--- pin-reminder.py
+++ pin-reminder.py
@@ -6,15 +6,18 @@ import configparser
 import requests
 import json
 import pandas
+import math
 from urllib3 import disable_warnings
 from urllib3.exceptions import InsecureRequestWarning
 disable_warnings(InsecureRequestWarning)
 
 headers = {
 	"content-type": "application/json",
-	"accept": "application/json",
-	"connection": "keep-alive"
+	"accept"      : "application/json",
+	"connection"  : "keep-alive"
 }
+
+today = datetime.datetime.today()
 
 def read_ini(file_path):
 	config = configparser.ConfigParser()
@@ -26,64 +29,81 @@ def read_ini(file_path):
 	}
 	return cfg
 
-cfg = read_ini("config.ini")
-
-print("Getting auth rules...")
-
-url = f"{cfg['base_url']}/vmrest/authenticationrules"
-response = requests.get(url=url, auth=cfg["creds"], headers=headers, verify=False)
-resp_json = response.json()
-
-authrules = []
-for r in resp_json["AuthenticationRule"]:
-	authrules.append({
-		"ObjectId"   : r["ObjectId"],
-		"DisplayName": r["DisplayName"],
-		"MaxDays"    : r["MaxDays"]
-	})
-
-# df = pandas.DataFrame(authrules)
-# print(df)
-
-print("Getting mailboxes data...")
-
-url = f"{cfg['base_url']}/vmrest/users"
-response = requests.get(url=url, auth=cfg["creds"], headers=headers, verify=False)
-resp_json = response.json()
-
-mailboxes = []
-for m in resp_json["User"]:
-	mailboxes.append({
-		"ObjectId"    : m["ObjectId"],
-		"Alias"       : m["Alias"],
-		"DisplayName" : m["DisplayName"],
-		"Extension"   : m["DtmfAccessId"],
-		"EmailAddress": m.get("EmailAddress", "")
-	})
-
-print(f"Total Mailboxes: {str(len(mailboxes))}")
-print("Getting mailbox PIN data...")
-
-today = datetime.datetime.today()
-
-for m in mailboxes:
-	url = f"{cfg['base_url']}/vmrest/users/{m['ObjectId']}/credential/pin"
-	response = requests.get(url=url, auth=cfg["creds"], headers=headers, verify=False)
+def get_auth_rules():
+	url       = f"{cfg['base_url']}/vmrest/authenticationrules"
+	response  = requests.get(url=url, auth=cfg["creds"], headers=headers, verify=False)
 	resp_json = response.json()
 
-	m["DoesntExpire"]     = resp_json["DoesntExpire"]
-	m["CredMustChange"]   = resp_json["CredMustChange"]
-	m["TimeChanged"]      = datetime.datetime.strptime(resp_json["TimeChanged"], "%Y-%m-%d %H:%M:%S.%f")
-	m["TimeExpired"]      = m["TimeChanged"] + datetime.timedelta(days=180)
-	m["DaysUntilExpired"] = m["TimeExpired"] - today
-	m["DaysUntilExpired"] = m["DaysUntilExpired"].days
-	for r in authrules:
-		if resp_json["CredentialPolicyObjectId"] == r["ObjectId"]:
-			m["AuthRule"] = r["DisplayName"]
-			break
+	authrules = []
+	for r in resp_json["AuthenticationRule"]:
+		authrules.append({
+			"ObjectId"   : r["ObjectId"],
+			"DisplayName": r["DisplayName"],
+			"MaxDays"    : r["MaxDays"]
+		})
+	return authrules
 
-x = datetime.datetime.now()
-# x2 = datetime.datetime.strptime()
+def get_mailboxes():
+	#! PAGINATION is WIP
+	url       = f"{cfg['base_url']}/vmrest/users?rowsPerPage=0"
+	response  = requests.get(url=url, auth=cfg["creds"], headers=headers, verify=False)
+	resp_json = response.json()
+	total_mailboxes = resp_json['@total']
+	print(f"Total Mailboxes: {total_mailboxes}")
+	rowsPerPage = 100
+	total_pages = math.ceil(int(total_mailboxes) / rowsPerPage)
+	print(f"total_pages = {total_pages}")
+	print("Starting page loop")
 
-df = pandas.DataFrame(mailboxes)
-print(df[['Alias', 'DoesntExpire', 'CredMustChange', 'TimeChanged', 'TimeExpired', 'DaysUntilExpired']])
+	for pageNumber in range(total_pages):
+		print(f"pageNumber = {pageNumber+1}")
+		url       = f"{cfg['base_url']}/vmrest/users?rowsPerPage={rowsPerPage}&pageNumber={pageNumber+1}"
+		response  = requests.get(url=url, auth=cfg["creds"], headers=headers, verify=False)
+		resp_json = response.json()
+
+		mailboxes = []
+		for m in resp_json["User"]:
+			mailboxes.append({
+				"ObjectId"     : m["ObjectId"],
+				"Alias"        : m["Alias"],
+				"Display Name" : m["DisplayName"],
+				"Extension"    : m["DtmfAccessId"],
+				"Email Address": m.get("EmailAddress", "")
+			})
+	return mailboxes
+
+def get_pin_data():
+	for m in mailboxes:
+		url       = f"{cfg['base_url']}/vmrest/users/{m['ObjectId']}/credential/pin"
+		response  = requests.get(url=url, auth=cfg["creds"], headers=headers, verify=False)
+		resp_json = response.json()
+
+		for r in authrules:
+			if resp_json["CredentialPolicyObjectId"] == r["ObjectId"]:
+				m["Auth Rule"]       = r["DisplayName"]
+				m["Expiration Days"] = r["MaxDays"]
+				break
+		m["PIN Doesnt Expire"]  = resp_json["DoesntExpire"]
+		m["PIN Must Change"]    = resp_json["CredMustChange"]
+		m["Date Last Changed"]  = datetime.datetime.strptime(resp_json["TimeChanged"], "%Y-%m-%d %H:%M:%S.%f")
+		m["Expiration Date"]    = m["Date Last Changed"] + datetime.timedelta(days=int(m["Expiration Days"]))
+		m["Days Until Expired"] = m["Expiration Date"] - today
+		m["Days Until Expired"] = m["Days Until Expired"].days
+		m["Date Last Changed"]  = m["Date Last Changed"].date() # Convert datetime to date
+		m["Expiration Date"]    = m["Expiration Date"].date()   # Convert datetime to date
+
+	df = pandas.DataFrame(mailboxes)
+	print(df[['Alias', 'PIN Doesnt Expire', 'PIN Must Change', 'Date Last Changed', 'Expiration Days', 'Expiration Date', 'Days Until Expired']])
+	return mailboxes
+
+if __name__ == "__main__":
+	cfg = read_ini("config.ini")
+
+	print("Getting auth rules...")
+	authrules = get_auth_rules()
+
+	print("Getting mailboxes...")
+	mailboxes = get_mailboxes()
+	
+	print("Getting PIN data...")
+	get_pin_data()
